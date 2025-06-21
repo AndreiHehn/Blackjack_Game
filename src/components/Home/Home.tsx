0e11ab9b8a60be280ebc233dcf6d8c2e7160f882
AndreiHehn <andrei.hehnpereira@gmail.com>
2025-06-20 21:36:19 -0400

    fix: icons not being displayed

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,17 +2,18 @@ import { Container } from "../../styles/Home.ts";
 
 import ClubsIcon from "../../assets/icons/ClubsIcon.svg?react";
 import SpadesIcon from "../../assets/icons/SpadesIcon.svg?react";
-// import HeartsIcon from "../../assets/icons/HeartsIcon.svg?react";
-// import DiamondsIcon from "../../assets/icons/DiamondsIcon.svg?react";
+import HeartsIcon from "../../assets/icons/HeartsIcon.svg?react";
+import DiamondsIcon from "../../assets/icons/DiamondsIcon.svg?react";
 
 export function Home() {
+  console.log("testeeeee");
   return (
     <Container>
-      <div>ye</div>
+      <div>teste</div>
       <ClubsIcon />
       <SpadesIcon />
-      {/* <HeartsIcon />
-      <DiamondsIcon /> */}
+      <HeartsIcon />
+      <DiamondsIcon />
     </Container>
   );
 }
