10d92680f70ae61bf7c56e130e41508dbd93d041
AndreiHehn <andrei.hehnpereira@gmail.com>
2025-06-20 21:25:15 -0400

    feat: config

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,18 @@
+import { Container } from "../../styles/Home.ts";
+
+import ClubsIcon from "../../assets/icons/ClubsIcon.svg?react";
+import SpadesIcon from "../../assets/icons/SpadesIcon.svg?react";
+// import HeartsIcon from "../../assets/icons/HeartsIcon.svg?react";
+// import DiamondsIcon from "../../assets/icons/DiamondsIcon.svg?react";
+
+export function Home() {
+  return (
+    <Container>
+      <div>ye</div>
+      <ClubsIcon />
+      <SpadesIcon />
+      {/* <HeartsIcon />
+      <DiamondsIcon /> */}
+    </Container>
+  );
+}
