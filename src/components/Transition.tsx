3931c341165b209f4065705d71ec64be94308999
AndreiHehn <andrei.hehnpereira@gmail.com>
2025-06-23 21:19:02 -0400

    translations: add

--- src/components/Transition.tsx
+++ src/components/Transition.tsx
@@ -4,8 +4,10 @@ import ClubsIcon from "../assets/icons/ClubsIcon.svg?react";
 import SpadesIcon from "../assets/icons/SpadesIcon.svg?react";
 import HeartsIcon from "../assets/icons/HeartsIcon.svg?react";
 import DiamondsIcon from "../assets/icons/DiamondsIcon.svg?react";
+import { useTranslation } from "react-i18next";
 
 export function TransitionPage() {
+  const { t } = useTranslation();
   return (
     <Container>
       <div className="loading">
@@ -15,7 +17,7 @@ export function TransitionPage() {
           <SpadesIcon className="suitsIcon spades" />
           <DiamondsIcon className="suitsIcon diamonds red" />
         </div>
-        <h2 className="loadingText">Carregando...</h2>
+        <h2 className="loadingText">{t("Loading")}...</h2>
       </div>
     </Container>
   );
